629c4bf0f849d14e561dbbecbfea56af06ce15ed
chaos29092 <wo94yun>
2014-10-29 15:44:27 +0800

    Initial commit

---
+++ app/views/index.blade.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="uzb"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="uzb"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="uzb"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"  dir="ltr" lang="uzb"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<!-- Set the viewport width to device width for mobile -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="author" content="chaos">
+<meta name="Description" content="Asfaltobeton zavodi, Asfalt va beton zavodi , Asfaltbeton zavodi eksport" />
+<link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
+<title>Hanvy - Asfaltobeton zavodi</title>
+<link href="{{asset('assets/css/reset.css')}}" rel="stylesheet" type="text/css" media="screen" />
+<link href="{{asset('assets/css/styles.css')}}" rel="stylesheet" type="text/css" media="screen" />
+<!--[if gt IE 8]><!--><link href="{{asset('assets/css/retina-responsive.css')}}" rel="stylesheet" type="text/css" media="screen" /><!--<![endif]-->
+<!--[if !IE]> <link href="{{asset('assets/css/retina-responsive.css')}}" rel="stylesheet" type="text/css" media="screen" /> <![endif]-->
+<!--[if lt IE 9]> <link href="{{asset('assets/css/styles-ie8.css')}}" rel="stylesheet" type="text/css" media="screen" /> <![endif]-->
+<link href="{{asset('assets/css/print.css')}}" rel="stylesheet" type="text/css" media="print" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,700,600,800' rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+<div class="container">
+  <div id="container" class="clearfix">
+    <nav class="col1-3 no-bg element home services clients portfolio about blog contact">
+      <div id="open-menu">
+       <img src="{{asset('img/logo.jpg')}}" alt="hanvy"/>
+
+      </div>
+      
+
+      <div id="logo-container">
+      <ul class="list check">
+                 <li>22-yillik ishlab chiqarish tajriba</li>
+                 <li>80ta mamlakatga 10 yillik eksport tajriba</li>
+                 <li>Narh va sifat raqobatbardoshligi</li>
+                 <li>Sotib olish so'roq varaqasi</li>
+               </ul>
+
+      </div>
+      </a>
+      </nav>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+              <div class="movable-content">
+                <div class="images"><img src="{{asset('img/Mobile-Asphalt-Mixing-Plant-1.jpg')}}" alt="" /></div>
+              </div>
+            </div>
+
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/asphalt-mixing-plant-1.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/Mobile-Asphalt-Mixing-Plant-2.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+    <div class="element home clearfix col2-3 services">
+               <div class="images"><img src="{{asset('img/canshu_1.jpg')}}" alt="" /></div>
+    </div>
+
+    <div class="element home clearfix col2-3 services">
+               <div class="images"><img src="{{asset('img/canshu_2.jpg')}}" alt="" /></div>
+    </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/asphalt-mixing-plant-2.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+    <div class="element home clearfix col2-3 services">
+               <div class="images"><img src="{{asset('img/canshu_3.jpg')}}" alt="" /></div>
+    </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/asphalt-mixing-plant-3.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+    <div class="element home clearfix col1-3 border services">
+      <h3>Bog'lanishingizni</h3>
+      <p>Modelning aniqlash va so'roq varag'ini olish uchun, biz bilan bog'lanishingizni so'raymiz.<br/><br/></p>
+
+    </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/Questionnaire_1.jpg')}}" alt="Questionnaire" /></div>
+             </div>
+        </div>
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/Questionnaire_2.jpg')}}" alt="Questionnaire" /></div>
+             </div>
+        </div>
+
+{{--客户参观工厂--}}
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/canguan/canguan_big_3.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/canguan/canguan_big_1.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/canguan/canguan_big_2.jpg')}}" alt="" /></div>
+            </div>
+        </div>
+
+{{--客户参观现场--}}
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/xianchang/xianchang_big_1.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/xianchang/xianchang_big_2.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/xianchang/xianchang_big_3.jpg')}}" alt="" /></div>
+            </div>
+        </div>
+
+{{--客户技术讨论--}}
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/taolun/taolun_big_1.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/taolun/taolun_big_2.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/taolun/taolun_big_3.jpg')}}" alt="" /></div>
+            </div>
+        </div>
+
+{{--客户验货--}}
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/zhanhui/zhanhui_big_1.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/zhanhui/zhanhui_big_2.jpg')}}" alt="" /></div>
+             </div>
+        </div>
+
+        <div class="element home clearfix col1-3 borders portfolio overflow">
+            <div class="movable-content">
+                <div class="images"><img src="{{asset('img/zhanhui/zhanhui_big_3.jpg')}}" alt="" /></div>
+            </div>
+        </div>
+
+</div>
+<!-- end header -->
+<footer>
+
+
+<div class="alignright">
+<h3>Свяжитесь с нами</h3>
+{{ Form::open(array('url' => 'mailPost')) }}
+    <div>
+    {{Form::label('email', 'E-Mail(*)  :')}}
+    {{Form::email('email',null,array('class'=>'contact_text','required'))}}
+    </div>
+    <div>
+    {{Form::label('subject', 'Name :')}}
+    {{Form::text('subject',null,array('class'=>'contact_text'))}}
+    </div>
+    <div>
+    {{Form::label('mes', 'Message:')}}
+    {{Form::textarea('mes',null,array('class'=>'contact_text'))}}
+    </div>
+    <div>{{Form::submit('Send Message')}}</div>
+{{ Form::close() }}
+</div>
+<div class="clearfix"></div>
+<p class="centered">© 2014, Zhengzhou Hanvy Machinery And Equipment Co., Ltd.</p>
+</footer>
+<!-- BACK TO TOP BUTTON -->
+<div id="backtotop">
+  <ul>
+    <li><a id="toTop" href="#" onClick="return false">Back to Top</a></li>
+  </ul>
+</div>
+</div>
+
+<script src="{{asset('assets/js/jquery-1.9.1.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/jquery-easing-1.3.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/modernizr.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/custom.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/jquery.gomap-1.3.2.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/jquery.isotope.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/jquery.ba-bbq.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/jquery.isotope.load_home.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/jquery.jtweetsanywhere-1.3.1.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('assets/js/scrollup.js')}}" type="text/javascript"></script>
+
+</body>
+</html>
